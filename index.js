88e5fd10d13cb1f1b45beafa3011ee4b48589323
ZloyPapan <botflint@yandex.ru>
2023-06-19 17:37:34 +0300

    add media

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const textNode = document.getElementById("text");
 const buttonNode = document.getElementById("button");
 
 buttonNode.addEventListener("click", function () {
-  fetch("http://www.boredapi.com/api/activity/")
+  fetch("https://www.boredapi.com/api/activity/")
     .then((response) => response.json())
     .then((res) => {
     //   if (res.status !== "success") {
